bed0e4d52198c6b81474b3ae727f9e97a014b593
Tóki Thorsteinsson <tt@fishfacts.fo>
2024-04-23 15:10:59 +0100

    Add harbors as one file

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# Toolbox for Fishfacts data analysis 
+# Toolbox for Fishfacts data analysis
+
+The repository currently contains the following packages:
+1. interpolating
+2. oil_calculation (better used in the fishfacts-data-analysis repo)
+3. plotting
+4. utils
+
+
--- zoning/zoning_oop.py
+++ zoning/zoning_oop.py
@@ -2,7 +2,12 @@ from time import time
 import pandas as pd
 from shapely.geometry import Point, Polygon
 import os
-from interpolating.interpolate import interpolate_dataframe
+
+
+class NamedPolygon:
+    def __init__(self, polygon: Polygon, name: str = None):
+        self.polygon = polygon
+        self.name = name
 
 
 class ZoneAssigner:
@@ -10,9 +15,9 @@ class ZoneAssigner:
         self.polygons = self.get_polygons_from_dir(polygons_directory)
 
     def which_zone(self, point):
-        for zone in self.polygons:
-            if point.within(zone[1]):
-                return zone[0]  # return name of zone
+        for polygon in self.polygons:
+            if point.within(polygon.polygon):
+                return polygon.name  # return name of zone
         return None
 
     def get_polygons_from_dir(self, directory):
@@ -20,17 +25,22 @@ class ZoneAssigner:
         for file_name in os.listdir(directory):
             if file_name.endswith('.csv'):
                 file_path = os.path.join(directory, file_name)
-                name, polygon = self.read_polygon_from_csv(file_path)
-                polygons.append((name, polygon))
+                polygon = self.read_polygon_from_csv(file_path)
+                polygons.append(polygon)
         return polygons
 
     def read_polygon_from_csv(self, csv_file_path):
         _df = pd.read_csv(csv_file_path)
         coordinates = [(row['latitude'], row['longitude']) for _, row in _df.iterrows()]
+
+        # Get name of file
         _, filename = os.path.split(csv_file_path)
         name, _ = os.path.splitext(filename)
-        polygon = Polygon(coordinates)
-        return name, polygon
+
+        # Initialize named polygon
+        polygon = NamedPolygon(Polygon(coordinates), name)
+
+        return polygon
 
     def assign_zones_to_dataframe(self, input_df):
         input_df['eez_zone'] = input_df.apply(
@@ -41,22 +51,11 @@ class ZoneAssigner:
 
 # Example usage
 if __name__ == "__main__":
-    time_read_start = time()
-    path = r"C:\Users\tokit\OneDrive\Desktop\Rapportir\Sæson rapportir\Sild 2023\Íslendsk\raw\vessel_Adalsteinn_Jonsson_20220101T1200-20230301T1200.xlsx"
+
+    path = r"/Users/tokithorsteinsson/Downloads/interpolated_vessel_476_Frank Bonefaas_20230702T0000-20230708T0000.xlsx"
     zones_dir = r'../zones/eez_zones'
+    pd.set_option('display.width', None)
     df = pd.read_excel(path)
-    time_read_end = time()
 
-    time_zone_start = time()
     df_with_zones = ZoneAssigner(zones_dir).assign_zones_to_dataframe(df)
-    time_zone_end = time()
-    print(df_with_zones.head())
-
-    read_time = time_read_end - time_read_start
-    zoning_time = time_zone_end - time_zone_start
-
-    print(f"Df read time:\t{read_time:10.4f} s")
-    print(f"Zn apply time:\t{zoning_time:10.4f} s")
-
-    print(f"Total time:\t\t{read_time+zoning_time:10.4f} s")
-    print(f"{df.shape[0]} rows")
+    print(df_with_zones.head(20))
