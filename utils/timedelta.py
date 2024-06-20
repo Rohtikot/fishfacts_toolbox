0726dd627566c96a0b83f71a93d008aa2bf4e2d0
Tóki Thorsteinsson <tt@fishfacts.fo>
2024-06-20 11:09:50 +0100

    Simplify timedelta function

--- utils/timedelta.py
+++ utils/timedelta.py
@@ -3,10 +3,7 @@ from pandas import DataFrame, Series
 
 def calculate_timedelta(input_df: DataFrame) -> Series:
     """Calculate the difference of time between consecutive rows"""
-    previous_time = input_df['timestamp'].shift()
-    time_deltas = input_df['timestamp'] - previous_time
-
-    return time_deltas
+    return input_df['timestamp'].diff()
 
 
 if __name__ == '__main__':
