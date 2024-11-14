c9e8a65da8bcca79dee7f5f5f4b94065aa06fc99
Tóki Thorsteinsson <tt@fishfacts.fo>
2024-11-14 11:22:05 +0000

    Changed names of dataframes inside functions.

--- vessels_close/vessels_meeting.py
+++ vessels_close/vessels_meeting.py
@@ -2,23 +2,23 @@ import pandas as pd
 from nautical_calculations.basic import get_distance
 
 
-def merge_df_on_timestamp(df1: pd.DataFrame, df2: pd.DataFrame, time_tolerance_minutes: int = 5) -> pd.DataFrame:
+def merge_df_on_timestamp(dataframe1: pd.DataFrame, dataframe2: pd.DataFrame, time_tolerance_minutes: int = 5) -> pd.DataFrame:
     """
     Merge two dataframes by aligning their timestamps to the nearest rounded timestamp.
 
-    :param df1: pandas AIS dataframe
-    :param df2: pandas AIS dataframe
+    :param dataframe1: pandas AIS dataframe
+    :param dataframe2: pandas AIS dataframe
     :param time_tolerance_minutes: int for minute tolerance
     :return: merged dataframe from both vessels with common column "timestamp_rounded"
     """
 
     # Make a rounded timestamp on each dataframe
-    df1['timestamp_rounded'] = df1['timestamp'].dt.round('5min')
-    df2['timestamp_rounded'] = df2['timestamp'].dt.round('5min')
+    dataframe1['timestamp_rounded'] = dataframe1['timestamp'].dt.round('5min')
+    dataframe2['timestamp_rounded'] = dataframe2['timestamp'].dt.round('5min')
 
     # Merge dataframes on the rounded timestamp column
-    merged_df = pd.merge_asof(df1.sort_values('timestamp_rounded'),
-                              df2.sort_values('timestamp_rounded'),
+    merged_df = pd.merge_asof(dataframe1.sort_values('timestamp_rounded'),
+                              dataframe2.sort_values('timestamp_rounded'),
                               on='timestamp_rounded',
                               direction='nearest',
                               tolerance=pd.Timedelta(f'{time_tolerance_minutes}min'))
@@ -28,9 +28,9 @@ def merge_df_on_timestamp(df1: pd.DataFrame, df2: pd.DataFrame, time_tolerance_m
     return merged_df
 
 
-def distance_between_vessels(input_df: pd.DataFrame) -> pd.Series:
+def distance_between_vessels(dataframe: pd.DataFrame) -> pd.Series:
     # Calculate the distance between rows in data frame in meters
-    distances = input_df.apply(
+    distances = dataframe.apply(
         lambda row: get_distance(
             row['latitude_x'],
             row['longitude_x'],
@@ -45,18 +45,18 @@ def distance_between_vessels(input_df: pd.DataFrame) -> pd.Series:
     return rolling_avg_distances
 
 
-def find_close_encounters(df1: pd.DataFrame, df2: pd.DataFrame, time_tolerance_minutes: int = 5) -> pd.DataFrame:
+def find_close_encounters(dataframe1: pd.DataFrame, dataframe2: pd.DataFrame, time_tolerance_minutes: int = 5) -> pd.DataFrame:
     """
     Find the close encounters between two vessels
 
-    :param df1: DataFrame of one vessel's AIS data
-    :param df2: DataFrame of the other vessel's AIS data
+    :param dataframe1: DataFrame of one vessel's AIS data
+    :param dataframe2: DataFrame of the other vessel's AIS data
     :param time_tolerance_minutes:  Time tolerance to align dataframes for
     :return:
     """
 
     # Merge both vessels' data frames on datetime column
-    merged_df = merge_df_on_timestamp(df1, df2, time_tolerance_minutes)
+    merged_df = merge_df_on_timestamp(dataframe1, dataframe2, time_tolerance_minutes)
 
     # Calculate the distance between each row of merged data frame
     merged_df['distance_m'] = distance_between_vessels(merged_df)
