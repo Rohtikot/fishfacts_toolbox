fb01cfe7561a1365e5807f4a5f815875d1a711b3
Tóki Thorsteinsson <tt@fishfacts.fo>
2024-12-19 11:30:06 +0000

    Add save function and add redundancy to scrape_vorn function so that it will save where it left off in case of error.

--- catches/far_catch_functions.py
+++ catches/far_catch_functions.py
@@ -36,14 +36,11 @@ def read_vorn_table(url: str, remove_empty_rows: bool = False) -> pd.DataFrame |
     Args:
         - url (str) - the URL for the catch data. This URL contains start date, end date and Vørn's vessel ID for
          each vessel.
-        - weight_unit (str) - 'kg' sets the weight units to kg, 'ton' set the weight unit to tonnes. Default: 'Ton'
-            can be set to 'dkk' to see the value of the landing
         - remove_empty_rows (bool) - truthy value removes rows with 0 catch, falsy value keeps rows with 0 catch.
 
     Returns:
         - df_columns (pd.DataFrame): A pandas DataFrame with columns 'Species' and
           'Vessel name - Harbour No. (Call sign)'
-            - Catch numbers can be in either kg or tonnes depending on 'weight_unit' argument. Default: 'Ton'
 
     Raises:
         ValueError: If the weight_unit argument is not 'kg' or 'ton'.
@@ -76,7 +73,7 @@ def read_vorn_table(url: str, remove_empty_rows: bool = False) -> pd.DataFrame |
         raise TypeError("The 'remove_empty_rows' argument must be a boolean.")
 
     headers = {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36'
     }
 
     # Retrieve content from URL
@@ -131,54 +128,98 @@ def read_vorn_table(url: str, remove_empty_rows: bool = False) -> pd.DataFrame |
         return df_columns  # return full data frame
 
 
-def scrape_vorn(vessel_group: VesselGroup or list[VesselGroup], start: datetime, end: datetime, mode: str) -> pd.DataFrame:
+def scrape_vorn(vessel_group: int or list[int], start: datetime, end: datetime, mode: str):
+    """
+    Function to scrape catch data from Vorn.fo.
+
+    :param vessel_group: value from VesselGroup class
+    :param start: datetime of start of period
+    :param end: datetime of end of period
+    :param mode: vektarseðil or avreiðingarseðil
+    :return: dataframe with catch data for specified vessel group
+    """
+
+    # Generate list of integers to be used in url
     vessel_group = generate_group(vessel_group)
+
+    # Generate list of dates
     dates = generate_date_range(start, end)
 
     dfs = []
 
-    for group in vessel_group:
-        group_num = group
-        group_name = group.name
-        for date in (pbar := tqdm(dates, total=len(dates))):
-            pbar.set_description(f'Processing: {date} - {group_name}')
-            url = generate_vorn_url(group_num, date, mode)
-            data = read_vorn_table(url, remove_empty_rows=True)
-            if data is not None:
-                data = data.drop('Tils. (kg)', axis=1)
-                data = data[data['species'] != 'Tils.']
-                data['date'] = date
+    try:
+        for group in vessel_group:
+            group_num = group
 
-                # Identify the vessel columns dynamically by excluding 'Species' and 'date'
-                vessel_columns = data.columns.difference(['species', 'date'])
+            group_name = VesselGroup(group_num).name
 
-                # Melt the DataFrame to unpivot vessel columns to rows
-                data_melted = pd.melt(
-                    data,
-                    id_vars=['species', 'date'],  # Columns to keep
-                    value_vars=vessel_columns,    # Columns to unpivot
-                    var_name='vessel_name',       # New column name for vessel
-                    value_name='value (kg)'       # New column name for catch value
-                )
+            for date in (pbar := tqdm(dates, total=len(dates))):
+                pbar.set_description(f'Processing: {date} - {group_name}')
+                url = generate_vorn_url(group_num, date, mode)
+                data = read_vorn_table(url, remove_empty_rows=True)
+                if data is not None:
+                    data = data.drop('Tils. (kg)', axis=1)
+                    data = data[data['species'] != 'Tils.']
+                    data['date'] = date
 
-                data_melted['vessel_type'] = group_name.lower()
+                    # Identify the vessel columns dynamically by excluding 'Species' and 'date'
+                    vessel_columns = data.columns.difference(['species', 'date'])
 
-                # Extract and clean the vessel name from the column names
-                data_melted['vessel_name'] = data_melted['vessel_name'].str.extract(r'^(.*) -')[0]
+                    # Melt the DataFrame to unpivot vessel columns to rows
+                    data_melted = pd.melt(
+                        data,
+                        id_vars=['species', 'date'],  # Columns to keep
+                        value_vars=vessel_columns,    # Columns to unpivot
+                        var_name='vessel_name',       # New column name for vessel
+                        value_name='value (kg)'       # New column name for catch value
+                    )
 
-                split_cols = data_melted['vessel_name'].str.split(r' \(', expand=True)
-                data_melted['vessel_name'] = split_cols[0].str.strip()
-                data_melted['radio_callsign'] = split_cols[1].str.replace(r'\)', '', regex=True).str.strip()
+                    data_melted['vessel_type'] = group_name.lower()
 
-                data_melted = data_melted[['date', 'vessel_name', 'radio_callsign', 'vessel_type', 'species', 'value (kg)']]
+                    # Extract and clean the vessel name from the column names
+                    data_melted['vessel_name'] = data_melted['vessel_name'].str.extract(r'^(.*) -')[0]
 
-                dfs.append(data_melted)
+                    split_cols = data_melted['vessel_name'].str.split(r' \(', expand=True)
+                    data_melted['vessel_name'] = split_cols[0].str.strip()
+                    data_melted['radio_callsign'] = split_cols[1].str.replace(r'\)', '', regex=True).str.strip()
 
-                sleep(1)
+                    data_melted = data_melted[['date', 'vessel_name', 'radio_callsign', 'vessel_type', 'species', 'value (kg)']]
 
-    dfs = pd.concat(dfs)
-    return dfs
+                    dfs.append(data_melted)
 
+                sleep(2)
+
+    # Handle keyboard interrupt
+    except KeyboardInterrupt:
+        print(f'Process interrupted by user.')
+
+    # Handle any other error
+    except Exception as e:
+        print(f'An error occurred: {e}.')
+
+    finally:
+        dfs = pd.concat(dfs)
+        current_time = datetime.strftime(datetime.now(), format='%Y-%m-%d %H:%M:%S')
+        latest_date = save_data(dataframe=dfs, mode=mode)
+        latest_date = datetime.strftime(latest_date, format='%Y-%m-%d')
+        print(f'Latest date of dataframe {latest_date}. Data saved at {current_time}.')
+
+
+def get_first_and_last_fo_catch_date() -> pd.DataFrame:
+    """Retrieve the first and last date for scraped data for each vessel group"""
+
+    path = r"C:\Program Files (x86)\Fishfacts\catch\faroe_islands\catch\avreiðingarseðil.xlsx"
+
+    df = pd.read_excel(path)
+    grouped = df.groupby('vessel_type')
+
+    _df = pd.DataFrame(columns=['group', 'start', 'end'])
+
+    for name, group in grouped:
+        start, end = group['date'].min(), group['date'].max()
+        _df.loc[len(_df)] = [name, start, end]
+
+    return _df
 
 
 def generate_date_range(start: datetime, end: datetime) -> list[datetime]:
@@ -208,17 +249,63 @@ def generate_vorn_url(vessel_group: int, date: datetime, mode: str) -> str:
     return url
 
 
-def generate_group(vessel_group: VesselGroup or list[VesselGroup]) -> list[VesselGroup]:
+def generate_group(vessel_group: int or list[int]) -> list[int]:
     """Determine if vessel group is string or list and return list of tuples"""
 
-    if isinstance(vessel_group, VesselGroup):
+    if isinstance(vessel_group, int):
         return [vessel_group]
     elif isinstance(vessel_group, list):
         return vessel_group
 
 
-if __name__ == '__main__':
-    start, end = datetime(2023, 1, 1), datetime(2023, 2, 1)
+def save_data(dataframe: pd.DataFrame, mode: 'str') -> datetime:
+    """Save dataframe to Faroese catches."""
 
-    res = scrape_vorn(VesselGroup.PELAGIC, start, end, 'avr')
-    print(res)
+    # Avreiðingar- or vektarseðil
+    if mode == 'avr':
+        path = r"C:\Program Files (x86)\Fishfacts\catch\faroe_islands\catch\avreiðingarseðil.xlsx"
+    elif mode == 'vekt':
+        path = r"C:\Program Files (x86)\Fishfacts\catch\faroe_islands\catch\vektarseðil.xlsx"
+
+    # Add data to existing dataframe
+    main_df = pd.read_excel(path)
+    main_df.set_index('date', inplace=True)
+    dataframe.set_index('date', inplace=True)
+
+    main_df = pd.concat([main_df, dataframe])
+    main_df.reset_index(inplace=True)
+    main_df.drop_duplicates(inplace=True)
+    main_df.to_excel(path)
+
+    # return the last date of the main data frame
+    return main_df['date'].max()
+
+
+if __name__ == '__main__':
+    turns = get_first_and_last_fo_catch_date()
+
+    def get_vessel_group_enum(group_value: str):
+        """
+        Converts a string representing a vessel group to the corresponding VesselGroup enum member.
+
+        :param group_value: String value from the 'group' column in the DataFrame (e.g., 'freezing_trawler').
+        :return: Corresponding VesselGroup enum member if found, else None.
+        """
+
+        enum_name = group_value.upper()
+        try:
+            return getattr(VesselGroup, enum_name)
+        except AttributeError:
+            return None
+
+    turns['vessel_group_num'] = turns['group'].apply(get_vessel_group_enum)
+
+    # for index, row in turns.iterrows():
+    #     vessel_type = row['vessel_group_num']
+    #     start_d = row['end']
+    #     end_d = datetime.now()
+    #     # print(vessel_type, 'here')
+    #     scrape_vorn(vessel_type, start_d, end_d, 'avr')
+    print(turns)
+    
+    scrape_vorn(VesselGroup.PELAGIC, datetime(2024, 9, 1), datetime.now(), 'avr')
